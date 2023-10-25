dd4b428ba0c71852b082a78f28dff3624af85c43
Clumsy_Cranberry_1 <mlsshrbst@gmail.com>
2023-10-25 19:09:37 +0200

    update schema validation options

--- joi-validation-schemas.js
+++ joi-validation-schemas.js
@@ -2,22 +2,48 @@
 const Joi = require("joi");
 
 module.exports.joiCampgroundSchema = Joi.object({
-	title: Joi.string().trim().required(),
-	location: Joi.string().required(),
-	description: Joi.string().max(300).required(),
-	price: Joi.number().integer().min(0).required(),
-	images: Joi.string().required(),
+	title:
+		Joi.string().
+			trim().
+			required(),
+	location:
+		Joi.string()
+			.required(),
+	description:
+		Joi.string()
+			.max(300)
+			.required(),
+	price: Joi.number()
+		.integer()
+		.min(0)
+		.required(),
+	images:
+		Joi.string().required(),
 });
 
 module.exports.joiReviewSchema = Joi.object({
-	comment: Joi.string().required(),
-	rating: Joi.number().min(0).max(5).required(),
+	comment: Joi.string()
+		.required()
+		.max(2500),
+	rating: Joi.number()
+		.min(1)
+		.max(5)
+		.required(),
 });
 
 module.exports.joiUserSchema = Joi.object({
-	username: Joi.string().alphanum().trim().required(),
-	email: Joi.string().email().required(),
-	password: Joi.string(),
+	username: Joi.string()
+		.alphanum()
+		.min(3)
+		.max(30)
+		.trim()
+		.required(),
+	email: Joi.string()
+		.email()
+		.required(),
+	password: Joi.string()
+		.regex(/^[a-zA-Z0-9]{3,30}$/)
+		.required()
 });
 
 
